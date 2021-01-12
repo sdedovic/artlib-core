49a1c6874ceced2ce25852a2bfcf3ece02a4293f
stevan <stevan@dedovic.com>
2021-01-12 08:11:14 -0600

    increment next release version

--- project-template/project.clj
+++ project-template/project.clj
@@ -1,7 +1,7 @@
 (defproject sketch "0.1.0-SNAPSHOT"
   :description "A template for a sketch with Quil/Processing"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.dedovic/artlib-core "0.0.1-SNAPSHOT"]
+                 [com.dedovic/artlib-core "0.0.2-SNAPSHOT"]
                  [genartlib/genartlib "0.1.21"]] ; utility functions
   :jvm-opts ["-Xms4000m" "-Xmx4000M" ; 4GB heap size
              "-server"
