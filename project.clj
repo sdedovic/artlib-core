5701a44ae856c5ac8bffbb9d86b92c86169e5069
Stevan Dedovic <stevan@Stevans-MacBook-Pro.local>
2021-08-09 13:26:35 -0500

    upgrade to Processing 4.x for OSX/Java11 compatibility

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.mikera/vectorz-clj "0.48.0"]
                  [progrock "0.1.2"]
-                 [quil "3.1.0"]]
+                 [quil "4.0.0-SNAPSHOT-1"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :resource-paths ["resources"]
