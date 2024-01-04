12a0a2d9dbe42f503af1073c90fc1a131329393e
sdedovic <stevan@dedovic.com>
2024-01-04 02:08:49 +0000

    add useful random function since genartlib is broken on clojure 1.11

--- src/clj/artlib/quil/global.clj
+++ src/clj/artlib/quil/global.clj
@@ -1,4 +1,5 @@
-(ns artlib.quil.global)
+(ns artlib.quil.global
+  (:require [quil.core :as q]))
 
 (defmacro with-matrix [& body]
   `(do
@@ -14,3 +15,12 @@
        ~@body
        (finally (q/pop-style)))))
 
+; TODO: move to new artlib.quil.random namespace?
+;; Copyright (c) 2016 Tyler Hobbs
+;; https://github.com/thobbs/genartlib/blob/master/LICENSE
+;; https://github.com/thobbs/genartlib/blob/master/src/clj/genartlib/random.clj#L6
+(defn gauss
+  "Samples a single value from a Gaussian distribution with the given mean
+   and variance"
+  [mean variance]
+  (+ mean (* variance (q/random-gaussian))))
