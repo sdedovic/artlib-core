79d5f8bde22f2f14fa165d0349caa7d5a94fb2c9
sdedovic <stevan@dedovic.com>
2025-01-07 01:12:42 -0600

    test and add triangle wave to waveforms

--- src/clj/artlib/waveform.clj
+++ src/clj/artlib/waveform.clj
@@ -23,15 +23,29 @@
 (defn cos
   "Cosine wave."
   [t]
-  (Math/sin t))
+  (Math/cos t))
 
 (defn cos-norm
   "Cosine wave, normalize output [0, 1]"
   [t]
-  (Math/fma (Math/sin t) 0.5 0.5))
+  (Math/fma (Math/cos t) 0.5 0.5))
 
-(let period
-(defn tri
-  "Triangle wave."
-  [t]
-  (Math/floor (+ (/ t p
+;; https://en.wikipedia.org/wiki/Triangle_wave
+(let [period Math/TAU
+      phase (* Math/PI 1/2)]
+
+  (defn tri
+    "Triangle wave. 
+      
+    Note: the input is phase shifted to output 0 for an input of 0."
+    [t]
+    (let [tp (/ (+ t phase) period)]
+      (Math/fma (abs (* 2 (- tp (Math/floor (+ tp 1/2))))) 2.0 -1.0)))
+
+  (defn tri-norm
+    "Triangle wave.
+
+    Note: the input is phase shifted to output 0 for an input of 0."
+    [t]
+    (let [tp (/ t period)]
+      (* 2 (abs (- tp (Math/floor (+ tp 1/2))))))))
---
+++ test/clj/artlib/waveform_test.clj
@@ -0,0 +1,44 @@
+(ns artlib.waveform-test
+  (:require [artlib.testutils :refer [eq-float]]
+            [artlib.waveform :as waveform]
+            [clojure.test :refer [is deftest]]))
+
+(deftest sin
+  (is (eq-float (waveform/sin 2.123) (Math/sin 2.123)))
+  (is (eq-float (waveform/sin 0)     (Math/sin 0)))
+  (is (eq-float (waveform/sin 31.3)  (Math/sin 31.3))))
+
+(deftest sin-norm
+  (is (eq-float (waveform/sin-norm 2.123) (+ 0.5 (* 0.5 (Math/sin 2.123)))))
+  (is (eq-float (waveform/sin-norm 0)     (+ 0.5 (* 0.5 (Math/sin 0)))))
+  (is (eq-float (waveform/sin-norm 31.3)  (+ 0.5 (* 0.5 (Math/sin 31.3))))))
+
+(deftest cos
+  (is (eq-float (waveform/cos 2.123) (Math/cos 2.123)))
+  (is (eq-float (waveform/cos 0)     (Math/cos 0)))
+  (is (eq-float (waveform/cos 31.3)  (Math/cos 31.3))))
+
+(deftest cos-norm
+  (is (eq-float (waveform/cos-norm 2.123) (+ 0.5 (* 0.5 (Math/cos 2.123)))))
+  (is (eq-float (waveform/cos-norm 0)     (+ 0.5 (* 0.5 (Math/cos 0)))))
+  (is (eq-float (waveform/cos-norm 31.3)  (+ 0.5 (* 0.5 (Math/cos 31.3))))))
+
+(deftest tri
+  (is (eq-float (waveform/tri 0)                  0))
+  (is (eq-float (waveform/tri (* Math/PI 1/2))    1))
+  (is (eq-float (waveform/tri Math/PI)            0))
+  (is (eq-float (waveform/tri (* Math/PI 3/2))    -1))
+  (is (eq-float (waveform/tri (* Math/PI 2))      0))
+  (is (eq-float (waveform/tri (* Math/PI 5/2))    1))
+  (is (eq-float (waveform/tri (* Math/PI 3))      0))
+  (is (eq-float (waveform/tri (* Math/PI 7/2))    -1)))
+
+(deftest tri-norm
+  (is (eq-float (waveform/tri-norm 0)                  0))
+  (is (eq-float (waveform/tri-norm (* Math/PI 1/2))    0.5))
+  (is (eq-float (waveform/tri-norm Math/PI)            1))
+  (is (eq-float (waveform/tri-norm (* Math/PI 3/2))    0.5))
+  (is (eq-float (waveform/tri-norm (* Math/PI 2))      0))
+  (is (eq-float (waveform/tri-norm (* Math/PI 5/2))    0.5))
+  (is (eq-float (waveform/tri-norm (* Math/PI 3))      1))
+  (is (eq-float (waveform/tri-norm (* Math/PI 7/2))    0.5)))
